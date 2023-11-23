d5a36fff5ee798dc501701b8a6563d03e98806eb
422UR4H <tiagosindra@id.uff.br>
2023-11-23 18:31:54 -0300

    :lipstick: Style: add themes

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#4D4494",
+    secondary: "#EC6E4C",
+
+    primaryBackground: "#D8D8D8",
+    secondaryBackground: "#F5F5F5",
+
+    primaryText: "#222222",
+    secondaryText: "#C8C8C8",
+    ternaryText: "#AFADAD",
+
+    link: "#96A7F2",
+  },
+};
