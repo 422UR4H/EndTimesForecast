81388d3731f51aff354cb63495fdbcadc7f69280
422UR4H <tiagosindra@id.uff.br>
2023-11-24 20:36:35 -0300

    :sparkles: Feat: temperature persisted state

--- src/components/organisms/SwitchBox/index.tsx
+++ src/components/organisms/SwitchBox/index.tsx
@@ -1,19 +1,35 @@
-import { DefaultTheme } from "styled-components/dist/types";
+import usePersistedState from "../../../hooks/usePersistedState";
 import SwitchWrapper from "../../molecules/SwitchWrapper";
 import StyledSwitchBox from "./styled";
+// import { useState } from "react";
 
 type SwitchBoxProps = {
-  theme: DefaultTheme;
+  value: string;
   toggleTheme(): void;
 };
 
-export default function SwitchBox({ theme, toggleTheme }: SwitchBoxProps) {
+export default function SwitchBox({ value, toggleTheme }: SwitchBoxProps) {
+  const [unit, setUnit] = usePersistedState<string>(
+    "temperatureUnit",
+    "celsius"
+  );
+
+  function toggleUnit() {
+    setUnit(unit === "celsius" ? "fahrenheit" : "celsius");
+  }
+
   return (
     <StyledSwitchBox>
-      <SwitchWrapper theme={theme} toggleTheme={toggleTheme}>
+      <SwitchWrapper
+        checked={unit === "fahrenheit"}
+        toggle={toggleUnit}
+      >
         °F
       </SwitchWrapper>
-      <SwitchWrapper theme={theme} toggleTheme={toggleTheme}>
+      <SwitchWrapper
+        checked={value === "dark"}
+        toggle={toggleTheme}
+      >
         Dark Mode
       </SwitchWrapper>
     </StyledSwitchBox>
--- src/components/organisms/SwitchBox/styled.ts
+++ src/components/organisms/SwitchBox/styled.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const StyledSwitchBox = styled.div`
-  
+  min-width: 250px;
+  display: flex;
+  flex-direction: column;
 `
 export default StyledSwitchBox;
