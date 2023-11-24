31001aca9a750b455759dbefacb678b2a57721c0
422UR4H <tiagosindra@id.uff.br>
2023-11-24 20:30:07 -0300

    :lipstick: Style: fine adjustments

--- src/components/atoms/Locality/styled.ts
+++ src/components/atoms/Locality/styled.ts
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 const StyledLocality = styled.div`
   h1 {
+    font-weight: 400;
     font-size: 150px;
+    line-height: 150px;
   }
 `;
 export default StyledLocality;
