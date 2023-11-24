437899092d7babb356fd6b93d1126419f60ce106
422UR4H <tiagosindra@id.uff.br>
2023-11-24 20:40:42 -0300

    :lipstick: Style: fine adjustments

--- src/styles/Line.ts
+++ src/styles/Line.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 const StyledLine = styled.div`
-  background-color: ${({ theme }) => theme.colors.primaryBackground}
+  background-color: ${({ theme }) => theme.colors.primaryBackground};
   width: 395px;
   height: 5px;
-  margin-top: 29px;
 `;
 export default StyledLine;
