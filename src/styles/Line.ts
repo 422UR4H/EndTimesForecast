8988fbb68e8e2aa070022c346d577b30f06ab56c
422UR4H <tiagosindra@id.uff.br>
2023-11-24 14:33:29 -0300

    :lipstick: Style: line

---
+++ src/styles/Line.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledLine = styled.div`
+  background-color: ${({ theme }) => theme.colors.primaryBackground}
+  width: 395px;
+  height: 5px;
+  margin-top: 29px;
+`;
+export default StyledLine;
