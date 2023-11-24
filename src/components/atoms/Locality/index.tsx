4837f2d629b7012c529452da47f2e1937285c875
422UR4H <tiagosindra@id.uff.br>
2023-11-24 14:28:59 -0300

    :sparkles: Feat: locality

---
+++ src/components/atoms/Locality/index.tsx
@@ -0,0 +1,10 @@
+import StyledLocality from "./styled";
+
+export default function Locality() {
+  return (
+    <StyledLocality>
+      <h1>São Paulo</h1>
+      <p>Lat: 44.34 Long: 10.99</p>
+    </StyledLocality>
+  );
+}
---
+++ src/components/atoms/Locality/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const StyledLocality = styled.div`
+  h1 {
+    font-size: 150px;
+  }
+`;
+export default StyledLocality;
