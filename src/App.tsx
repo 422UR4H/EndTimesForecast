b0e55ba2a16c1db2539a9f6f9e152ea115d53999
422UR4H <tiagosindra@id.uff.br>
2023-11-24 14:35:34 -0300

    :sparkles: Fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,34 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+import { useEffect } from "react";
+import GlobalStyles from "./styles/global";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 import Swal from "sweetalert2";
+import Switch from "react-switch";
+import usePersistedState from "./hooks/usePersistedState";
+import HomePage from "./pages/HomePage";
 
-type Todos = {
-  userId: number;
-  id: number;
-  title: "string";
-  completed: boolean;
-};
+// type Todos = {
+//   userId: number;
+//   id: number;
+//   title: "string";
+//   completed: boolean;
+// };
 
 function App() {
-  const [todos, setTodos] = useState<Todos[]>([]);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+  // const themeContext = useContext(ThemeContext);
 
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  function loadData() {
-    const URL = import.meta.env.VITE_API_URL;
-    axios
-      .get(`${URL}/todosa`)
-      .then(({ data }) => {
-        setTodos(data);
-      })
-      .catch((e) => {
-        Swal.fire({
-          title: "Ooops",
-          text: "Something went wrong",
-          timer: 5000,
-          showCloseButton: true,
-          confirmButtonText: "Okay...",
-          confirmButtonColor: "red",
-          icon: "error",
-        });
-        console.log(e.message);
-      });
+  function toggleTheme() {
+    setTheme(theme.title === 'light' ? dark : light);
   }
 
   return (
-    <>
-      {todos.length > 0 ? (
-        <ul>
-          {todos?.map((t) => (
-            <li key={t.id}>
-              <input type="checkbox" checked={t.completed} readOnly />
-              {t.title}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Await...</p>
-      )}
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <HomePage theme={theme} toggleTheme={toggleTheme} />
+    </ThemeProvider>
   );
 }
 
