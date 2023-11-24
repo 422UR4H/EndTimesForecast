64c4dea14b4879b9d2be56d413cebd771be1ba9d
422UR4H <tiagosindra@id.uff.br>
2023-11-24 14:28:38 -0300

    :sparkles: Feat: date

---
+++ src/components/atoms/Date/StyledDate.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const StyledDate = styled.div`
+  text-align: center;
+`;
+export default StyledDate;
---
+++ src/components/atoms/Date/index.tsx
@@ -0,0 +1,10 @@
+import StyledDate from "./StyledDate";
+
+export default function Date() {
+  return (
+    <StyledDate>
+      <p>16/11/2023</p>
+      <p>Quinta-feira, 16:32</p>
+    </StyledDate>
+  );
+}
