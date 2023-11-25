12f9d37b7e0739cb004b488fc01be08736f20cac
422UR4H <tiagosindra@id.uff.br>
2023-11-25 01:27:03 -0300

    :iphone: Style: add responsivity

--- src/components/atoms/Locality/styled.ts
+++ src/components/atoms/Locality/styled.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const StyledLocality = styled.div`
   h1 {
     font-weight: 400;
-    font-size: 150px;
-    line-height: 150px;
+    font-size: 7dvw;
+    line-height: 7dvw;
   }
 `;
 export default StyledLocality;
