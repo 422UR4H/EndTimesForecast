a9a3b91a9bcb357c368cf6e49ee2cefc5d676b84
422UR4H <tiagosindra@id.uff.br>
2023-11-27 06:13:28 -0300

    :construction: Fix: fix construction info

--- src/components/molecules/Info.tsx/index.tsx
+++ src/components/molecules/Info.tsx/index.tsx
@@ -1,12 +1,12 @@
 import StyledLine from "../../../styles/Line";
-import Temperature from "../../atoms/Temperature";
+// import Temperature from "../../atoms/Temperature";
 import Date from "../../atoms/MainDate";
 import StyledInfo from "./styled";
 
 export default function Info() {
   return (
     <StyledInfo>
-      <Temperature />
+      {/* <Temperature /> */}
       <StyledLine />
       <Date />
     </StyledInfo>
