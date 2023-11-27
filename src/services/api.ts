83c06b415fd66265bd7483e01c7e0ca3c5e777e4
422UR4H <tiagosindra@id.uff.br>
2023-11-27 00:40:33 -0300

    :sparkles: Feat: api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from "axios";
 
-export const geocodingApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-});
+async function getGeocoding(city: string, country = "Brasil")/*: Promise<AxiosResponse | AxiosError | void> */{
+  const URL = import.meta.env.VITE_GEOCODING_API_URL;
+  const KEY = import.meta.env.VITE_GEOCODING_API_KEY;
+  return axios.get(`${URL}q=${city},+${country}&no_annotations=1&key=${KEY}`);
+}
 
-export const openWeatherApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-});
+function getWeather(lat: number | string, lon: number | string) {
+  const URL = import.meta.env.VITE_OPEN_WEATHER_API_URL;
+  const KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+  return axios.get(`${URL}lat=${lat}&lon=${lon}&appid=${KEY}`);
+}
+
+const api = {
+  getWeather,
+  getGeocoding,
+};
+export default api;
