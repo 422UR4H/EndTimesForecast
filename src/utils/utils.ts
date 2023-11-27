8f4fe6f0a477b2517d5367e78c77c945c0deae4d
422UR4H <tiagosindra@id.uff.br>
2023-11-27 05:50:40 -0300

    :sparkles: Feat: format functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,5 +8,32 @@ function errorAlert(): void {
     // footer: '<a href="#">Por que eu estou vendo esse problema?</a>',
   });
 }
-const utils = { errorAlert };
+
+function getConvertedTemperature(kelvin: number, unit: string): string {
+  if (unit === "celsius") return convertKelvinToCelsius(kelvin);
+  return convertKelvinToFahrenheit(kelvin);
+}
+
+function convertKelvinToCelsius(kelvin: number): string {
+  console.log(kelvin);
+  return Math.round(kelvin - 273.15) + "°C";
+}
+
+function convertKelvinToFahrenheit(kelvin: number): string {
+  console.log(kelvin);
+  return Math.round(((kelvin - 273.15) * 9) / 5 + 32) + "°F";
+}
+
+function dateFormat(date: string | Date, joinChar = '-') {
+  if (typeof date !== 'string') date = date.toISOString();
+  return date.split('T')[0].split('-').reverse().join(joinChar);
+}
+
+const utils = {
+  errorAlert,
+  getConvertedTemperature,
+  convertKelvinToCelsius,
+  convertKelvinToFahrenheit,
+  dateFormat,
+};
 export default utils;
