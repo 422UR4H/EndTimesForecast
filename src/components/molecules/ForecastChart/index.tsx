141db9d50311166669d6ddcdf29541890412a46d
422UR4H <tiagosindra@id.uff.br>
2023-12-01 17:14:05 -0300

    :lipstick: Style: fix chart height

--- src/components/molecules/ForecastChart/index.tsx
+++ src/components/molecules/ForecastChart/index.tsx
@@ -37,7 +37,7 @@ export default function ForecastChart({
   });
 
   return (
-    <ResponsiveContainer width="100%" height={520}>
+    <ResponsiveContainer width="100%" height={450}>
       <LineChart
         style={{ backgroundColor: secondaryBackgroundColor }}
         data={formattedForecastData}
